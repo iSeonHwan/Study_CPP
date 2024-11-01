10433452a1bca0dcf6d7c295c4c74e1fc6617efd
iseonhwan <84608929+iSeonHwan@users.noreply.github.com>
2024-11-01 22:26:53 +0900

    Update alg_includes_ex1.cpp

--- Examples/alg_includes_ex1.cpp
+++ Examples/alg_includes_ex1.cpp
@@ -8,4 +8,19 @@ Date: 2024. 11. 1.
 #include <functional> // For greater<int>( )
 #include <iostream>
 
+int main()
+{
+  using namespace std;
+  vector<int> v1a, v1b;
+  vector<int>::iterator Iter1a, Iter1b;
 
+  v1a.push_back(1);
+  v1a.push_back(2);
+
+  v1b.push_back(1);
+  v1b.push_back(2);
+
+  bool Result1;
+  Result1 = includes(v1a.begin(), v1a.end(), v1b.begin(), v1b.end());
+  
+}
