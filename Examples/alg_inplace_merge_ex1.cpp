13a4196aed01343a1964db7bf5ea899e663cef0c
iseonhwan <84608929+iSeonHwan@users.noreply.github.com>
2024-11-03 22:30:18 +0900

    Create alg_inplace_merge_ex1.cpp

---
+++ Examples/alg_inplace_merge_ex1.cpp
@@ -0,0 +1,35 @@
+/*
+Define: inplace_merge example code
+date:24. 11. 3.
+*/
+
+/*
+template<class BidirectionalIterator>
+void inplace_merge(
+  BidirectionalIterator first,
+  BidirectionalIterator middle,
+  BidirectionalIterator last);
+
+template<class BidirectionalIterator, class Compare>
+void inplace_merge(
+  BidirectionalIterator first,
+  BidirectionalIterator middle,
+  BidirectionalIterator last,
+  **Compare pred**);
+
+template<**class ExecutionPolicy**, class BidirectionalIterator>
+void inplace_merge(
+  ExecutionPolicy&& exec,
+  BidirectionalIterator first,
+  BidirectionalIterator middel,
+  BidirectionalIterator last);
+
+template<class ExecutionPolicy, class BidirectionalIterator, **class Compare**>
+void inplace_merge(
+  ExecutionPolicy&& exec,
+  BidirectionalIterator first,
+  BidirectionalIterator middle,
+  BidirectionalIterator last,
+  Compare pred);
+
+*/
