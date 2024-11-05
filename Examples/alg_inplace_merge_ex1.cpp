c457740d02c644161a26c3b2b313c4f39bc598eb
iseonhwan <84608929+iSeonHwan@users.noreply.github.com>
2024-11-05 22:30:59 +0900

    Update alg_inplace_merge_ex1.cpp

--- Examples/alg_inplace_merge_ex1.cpp
+++ Examples/alg_inplace_merge_ex1.cpp
@@ -1,43 +1,12 @@
 /*
 Define: inplace_merge example code
-date:24. 11. 3.
-*/
-
-/*
-template<class BidirectionalIterator>
-void inplace_merge(
-  BidirectionalIterator first,Original" << end;
-      |     ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~
-  BidirectionalIterator middle,
-  BidirectionalIterator last);
-
-template<class BidirectionalIterator, class Compare>
-void inplace_merge(
-  BidirectionalIterator first,
-  BidirectionalIterator middle,
-  BidirectionalIterator last,
-  **Compare pred**);
-
-template<**class ExecutionPolicy**, class BidirectionalIterator>
-void inplace_merge(
-  ExecutionPolicy&& exec,
-  BidirectionalIterator first,
-  BidirectionalIterator middel,
-  BidirectionalIterator last);
-
-template<class ExecutionPolicy, class BidirectionalIterator, **class Compare**>
-void inplace_merge(
-  ExecutionPolicy&& exec,
-  BidirectionalIterator first,
-  BidirectionalIterator middle,
-  BidirectionalIterator last,
-  Compare pred);
-
+date:24. 11. 5.
 */
 
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <functional> //For greater<int>()
 
 int main()
 {
@@ -51,6 +20,9 @@ int main()
     v1.push_back(3);
     v1.push_back(2);
     v1.push_back(1);
+
+    v1.push_back(0);
+    
     v1.push_back(4);
     v1.push_back(5);
     v1.push_back(6);
@@ -58,23 +30,37 @@ int main()
 
 
     //print original
-    cout << "Print Original vector 1" << endl;
+    cout << "Print Original v1" << endl;
     for(Iter1 = v1.begin(); Iter1 != v1.end(); Iter1++)
     {
         cout << *Iter1 << " ";        
     }
-    cout << endl;
+    cout << endl << endl;
     
-    vector<int>::iterator break1;
-    break1 = find(v1.begin(), v1.end(), 1);
-    inplace_merge(v1.begin(), break1, v1.end());
+    //Construct v2
+    vector<int> v2 (v1);
+    vector<int>::iterator break2;
+    break2 = find (v2.begin(), v2.end(), 0); //find value 0 in v2
+    sort(v2.begin(), break2, greater<int>());
+    sort(break2, v2.end(), greater<int>());
+    cout << "v2(sorting around 0): " << endl;
+
+    vector<int>::iterator Iter2;
+    for(Iter2 = v2.begin(); Iter2 != v2.end(); Iter2++)
+        {
+            cout << *Iter2 << " ";
+        }
+
+    //line-break
+    cout << endl << endl;
     
-    //result print
-    cout << "inplace_merge result" << endl;
-    for(Iter1 = v1.begin(); Iter1 != v1.end(); Iter1++)
-    {
-        cout << *Iter1 << " ";
-    }
+    //inplace_merge v2
+    inplace_merge( v2.begin(), break2, v2.end(), greater<int>());
+    cout << "inplace_merge of v2:" << endl;
+    for(Iter2 = v2.begin(); Iter2 != v2.end(); Iter2++)
+        {
+            cout << *Iter2 << " ";
+        }
     
     return 0;
 }
